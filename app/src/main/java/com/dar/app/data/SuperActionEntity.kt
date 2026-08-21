6305a87329ee982290b8dcdf165a0b23fcc1d255
temporary-2731 <temporary25209@gmail.com>
2026-08-21 09:39:22 -0300

    Create SuperActionEntity data class for Room database

---
+++ app/src/main/java/com/dar/app/data/SuperActionEntity.kt
@@ -0,0 +1,14 @@
+package com.dar.app.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "super_action")
+data class SuperActionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val dslaId: Long,
+    val description: String = "",
+    val startDate: String? = null,
+    val endDate: String? = null
+)
