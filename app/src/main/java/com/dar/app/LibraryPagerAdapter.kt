fe1f1a0561174ab761ab1a63cc036bb399344073
temporary-2731 <temporary25209@gmail.com>
2026-08-22 02:57:00 -0300

    Add LibraryPagerAdapter for fragment management

---
+++ app/src/main/java/com/dar/app/LibraryPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.dar.app
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class LibraryPagerAdapter(
+    activity: FragmentActivity,
+    private val dslaId: Long
+) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> ActionListFragment.newInstance(dslaId)
+            1 -> GeneralActionListFragment.newInstance(dslaId)
+            2 -> SuperActionListFragment.newInstance(dslaId)
+            else -> ActionListFragment.newInstance(dslaId)
+        }
+    }
+}
