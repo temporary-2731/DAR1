71b4885e948bd1becb05ef84f2d42e989280d2d8
temporary-2731 <temporary25209@gmail.com>
2026-08-23 08:26:15 -0300

    Bump database version from 3 to 4

--- app/src/main/java/com/dar/app/data/AppDatabase.kt
+++ app/src/main/java/com/dar/app/data/AppDatabase.kt
@@ -15,7 +15,7 @@ import androidx.room.RoomDatabase
         SuperActionGeneralCrossRef::class,
         RecordingRow::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
