856a72eae1991504425288210eef87247d1b682c
temporary-2731 <temporary25209@gmail.com>
2026-08-26 04:10:04 -0300

    Increase item count in LibraryPagerAdapter to 4

--- app/src/main/java/com/dar/app/LibraryPagerAdapter.kt
+++ app/src/main/java/com/dar/app/LibraryPagerAdapter.kt
@@ -9,13 +9,14 @@ class LibraryPagerAdapter(
     private val dslaId: Long
 ) : FragmentStateAdapter(activity) {
 
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> ActionListFragment.newInstance(dslaId)
             1 -> GeneralActionListFragment.newInstance(dslaId)
             2 -> SuperActionListFragment.newInstance(dslaId)
+            3 -> RecoveryFragment.newInstance(dslaId)
             else -> ActionListFragment.newInstance(dslaId)
         }
     }
