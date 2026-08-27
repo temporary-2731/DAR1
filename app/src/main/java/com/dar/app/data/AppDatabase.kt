01f9de7d73540f08101474bccc36f0c4a3d1f692
temporary-2731 <temporary25209@gmail.com>
2026-08-27 04:37:43 -0300

    Increment database version from 5 to 6

--- app/src/main/java/com/dar/app/data/AppDatabase.kt
+++ app/src/main/java/com/dar/app/data/AppDatabase.kt
@@ -15,7 +15,7 @@ import androidx.room.RoomDatabase
         SuperActionGeneralCrossRef::class,
         RecordingRow::class
     ],
-    version = 5,
+    version = 6,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
