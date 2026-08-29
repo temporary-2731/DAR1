df27001037e39895569fb5bddde9f12f08efe925
temporary-2731 <temporary25209@gmail.com>
2026-08-29 17:35:51 -0300

    Increment database version from 8 to 9

--- app/src/main/java/com/dar/app/data/AppDatabase.kt
+++ app/src/main/java/com/dar/app/data/AppDatabase.kt
@@ -17,7 +17,7 @@ import androidx.room.RoomDatabase
         AnalysisForm::class,
         AnalysisFormActionParam::class
     ],
-    version = 8,
+    version = 9,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
